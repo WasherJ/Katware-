150c14ce869c14ac8521c2eb611ec8dac8bd7274
Sell <93877138+WasherJ@users.noreply.github.com>
2022-02-05 12:24:22 +0000

    Update KnifeBot.lua

--- KnifeBot.lua
+++ KnifeBot.lua
@@ -7,5 +7,5 @@
 -- go get them, edit them, do whatever with them. Just make sure to give credit.
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/30dd1189a8aeb23e188a696efd962e269250b073/ChatSpam.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/BoatRollerz/special-disco/main/KATSlient.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/BoatRollerz/special-disco/main/KATSlient.lua'))() -- works no?
 loadstring(game:HttpGet('https://raw.githubusercontent.com/cOpEtriNe/Katware-/main/virtualClick.lua'))()
